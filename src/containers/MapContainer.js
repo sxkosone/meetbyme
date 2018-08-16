681bd3f803ac9715a88beccb2b56afd7ab5a91c9
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-08-16 15:12:03 -0400

    some minor changes

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -3,7 +3,7 @@ import Map from '../components/Map'
 
 class MapContainer extends React.Component {
     render() {
-        return <div><Map /></div>
+        return <div>Map container<Map /></div>
     }
 }
 
