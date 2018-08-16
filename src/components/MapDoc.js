48e0638a39dce3033a7e3573d17e551a278f9111
github username <github email address>
2018-08-16 17:10:38 -0400

    no reactfragments in map

--- src/components/MapDoc.js
+++ src/components/MapDoc.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactMapboxGl, { Layer, Feature, Popup } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, Popup} from "react-mapbox-gl";
 
-
+const mapBoxToken= "pk.eyJ1Ijoid2xleTMzMzciLCJhIjoiY2prd2p2amxnMGlndDN3bzVieWE0N2dsbyJ9.aSerRKvNmEuVsN5R4k174Q"
+const style = "mapbox://styles/wley3337/cjkwtf1sp1a8b2ro0ojr9f8oe"
 
 const Map = ReactMapboxGl({
     accessToken: mapBoxToken
@@ -12,46 +13,32 @@ class MapDoc extends React.Component {
         console.log("clicked")
     }
 
-    // handleOnHover = (eventObj) => {
-    //     console.log(eventObj);
+    handleOnHover = (eventObj) => {
+        console.log(eventObj);
        
-    //     <Popup
-    //         coordinates={[eventObj.venue.lon, eventObj.venue.lat]}
-    //         offset={{
-    //             'bottom-left': [12, -38],  'bottom': [0, -38], 'bottom-right': [-12, -38]
-    //         }}
-    //         key={eventObj.id}
-    //         >
-    //         <h1>{eventObj.name}</h1>
-    //     </Popup>
-    // }
+        // <Popup
+        //     coordinates={[eventObj.venue.lon, eventObj.venue.lat]}
+        //     offset={{
+        //         'bottom-left': [12, -38],  'bottom': [0, -38], 'bottom-right': [-12, -38]
+        //     }}
+        //     key={eventObj.id}
+        //     >
+        //     <h1>{eventObj.name}</h1>
+        // </Popup>
+    }
 
 
     createEventMarkers= () =>{
-      
-        return this.props.events.results.map(eventObj => {
+        return this.props.events.results.map(eventObj => { 
             return(
-                  
-                        <React.Fragment>
-                            <Feature  
-                                properties={eventObj} 
-                                coordinates={[eventObj.venue.lon, eventObj.venue.lat]} 
-                                onClick={() => this.handleOnClick(eventObj)}
-                                // onMouseEnter={() => this.handleOnHover(eventObj)}
-                                key={`marker-${eventObj.id}`}
-                            />
-                            {/* <Popup
-                                coordinates={[eventObj.venue.lon, eventObj.venue.lat]}
-                                offset={{
-                                    'bottom-left': [12, -38],  'bottom': [0, -38], 'bottom-right': [-12, -38]
-                                }}
-                                key={`popup-${eventObj.id}`}
-                                >
-                                <h1>{eventObj.name}</h1>
-                            </Popup> */}
-                        </React.Fragment>
-
                         
+                <Feature  
+                    properties={eventObj} 
+                    coordinates={[eventObj.venue.lon, eventObj.venue.lat]} 
+                    onClick={() => this.handleOnClick(eventObj)}
+                    onMouseEnter={() => this.handleOnHover(eventObj)}
+                    key={`marker-${eventObj.id}`}
+                />                
             )
         })
     }
@@ -59,7 +46,7 @@ class MapDoc extends React.Component {
     render() {
         return( 
         <div>
-        { this.props.long ? 
+        { (this.props.long && this.props.events.length !== 0) ? 
              <Map
              //this is where you set the initial style properties of the map. Style is built on MapBox
                 style={`${style}`}
@@ -71,11 +58,12 @@ class MapDoc extends React.Component {
                 center={[this.props.long, this.props.lat]}
                 zoom ={[14]} //starting zoom level 0=far away, 20= very close
              >
+             
                 <Layer
                     type="symbol"
                     id="me"
                     layout={{ "icon-image": "marker-15" }}>
-                    <Feature coordinates={[this.props.long, this.props.lat]}/>
+                    <Feature coordinates={[this.props.long, this.props.lat]}/>                    
                 </Layer>
 
                 <Layer
