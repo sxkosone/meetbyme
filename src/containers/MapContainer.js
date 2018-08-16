ee078c1bf33620401acf1c68d4e3aaada9d3eda5
github username <github email address>
2018-08-16 15:17:44 -0400

    all good

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -3,11 +3,7 @@ import MapDoc from '../components/Map'
 
 class MapContainer extends React.Component {
     render() {
-<<<<<<< HEAD
-        return <div>Map container<Map /></div>
-=======
         return <div><MapDoc long={this.props.long} lat={this.props.lat} events={this.props.events}/></div>
->>>>>>> f2af4f6c084b44de9061a63e9dbcf479857339ec
     }
 }
 
